1611ba63df60a313ee2543e911f2d00a2c4c04a4
Zulfi Rizkiawan <zulfi.rizkiawan.27@gmail.com>
2020-11-25 13:10:07 +0700

    rental mobil

---
+++ OneDrive/Documents/NetBeansProjects/Rental/Rentalmobil/src/Database/Parameter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+/**
+ *
+ * @author zulfi
+ */
+public class Parameter {
+     public static String IPHOST="127.0.0.1";
+    public static String HOST_DB="jdbc:mysql://"+IPHOST+":3306/rental";
+    public static String USERNAME_DB="root";
+    public static String PASSWORD_DB="";
+    public static int PORT=11111;
+    public static String USER;
+}
---
+++ OneDrive/Documents/NetBeansProjects/Rental/Rentalmobil/src/Koneksi/Koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author zulfi
+ */
+public class Koneksi {
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/rental", "root","");
+                System.out.println("Berhasil");
+            } catch (SQLException ex) {
+                System.out.println("Error koneksi");
+            }
+        }
+        return connection;
+    }
+}
