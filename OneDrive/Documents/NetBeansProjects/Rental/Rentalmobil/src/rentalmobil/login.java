5b3e2aabaf35fe57b1306a466c113489ad6b6d46
PETER <PETER@LAPTOP-E2LQKFH7>
2020-12-01 09:22:51 +0700

    cobaaa

--- OneDrive/Documents/NetBeansProjects/Rental/Rentalmobil/src/rentalmobil/login.java
+++ OneDrive/Documents/NetBeansProjects/Rental/Rentalmobil/src/rentalmobil/login.java
@@ -17,7 +17,7 @@ import javax.swing.JOptionPane;
  * @author zulfi cogan 
  */
 public class login extends javax.swing.JFrame {
-// peter baik
+// peter ganteng
     /**
      * Creates new form login
      */
